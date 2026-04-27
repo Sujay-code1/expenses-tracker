f7c471f6945876bea131fe2a19345b746c16dcbc
Sujay-code1 <alexsujay111@gmail.com>
2026-04-27 21:58:26 +0530

    samesite none

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -79,7 +79,7 @@ export const Login = async (req, res) => {
       .cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000
       })
       .json({
